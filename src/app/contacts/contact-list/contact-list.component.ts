362bcab7d49b798580a43e34bfcb6b9e156454b4
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-05-06 08:19:05 -0600

    steps for contact details

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -7,9 +7,16 @@ import { Contact } from '../contact.model';
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     new Contact(1, 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'assets/images/jacksonk.jpg', null),
     new Contact(2, 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', 'assets/images/barzeer.jpg', null)
   ];
+
+  ngOnInit() {
+  }
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 }
