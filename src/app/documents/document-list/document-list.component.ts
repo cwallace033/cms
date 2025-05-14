76e0ee9e009507aae55553cf333652fb04286aa6
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-05-14 07:38:22 -0600

    added documents and dropdown

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
+
+  constructor() { }
+  ngOnInit(): void {}
+  documents: Document[] = [
+    new Document(1, 'Document 1', ' A description 1 of document 1 that details what exactly document 1 has to offer for the client.', ' https://example.com/doc1', []),
+    new Document(2, 'Document 2', ' A description 2 of document 2 that details what exactly document 2 has to offer for the client.', ' https://example.com/doc2', []),
+    new Document(3, 'Document 3', ' A description 3 of document 3 that details what exactly document 3 has to offer for the client.', ' https://example.com/doc3', []),
+    new Document(4, 'Document 4', ' A description 4 of document 4 that details what exactly document 4 has to offer for the client.', ' https://example.com/doc4', []),
+  ]
+  
 }
